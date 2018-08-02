a0b05b29dcaa1afddbaa8a25186d476762d1547a
Kamilla Kovacs <knkovacs@gmail.com>
2018-08-02 17:32:06 +0200

    final upload for day

--- week-01/day-4/first.ts
+++ week-01/day-4/first.ts
@@ -143,4 +143,4 @@ for(z = 1; z < 101; z++) {
 // Write a program that prints the numbers from 1 to 100.
 // But for multiples of three print “Fizz” instead of the number
 // and for the multiples of five print “Buzz”.
-// For numbers which are multiples of both three and five print “FizzBuzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”
