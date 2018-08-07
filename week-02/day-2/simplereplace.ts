fae5e194ffd0d97773e69e89b2b47d9514ce3298
Kamilla Kovacs <knkovacs@gmail.com>
2018-08-07 11:05:04 +0200

    uploading bubble, unique, and simplereplace into day-1 / day-2

---
+++ week-02/day-2/simplereplace.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let example: string = 'In a dishwasher far far away';
+
+let newExample: string = example.replace('dishwasher', 'galaxy');
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+console.log(newExample);
+
+export = example;
