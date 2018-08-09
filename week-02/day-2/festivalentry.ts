5cad1c83a63cf44f785f691bd21d62324e1345e0
Kamilla Kovacs <knkovacs@gmail.com>
2018-08-09 13:29:16 +0200

    festival with no booze

--- week-02/day-2/festivalentry.ts
+++ week-02/day-2/festivalentry.ts
@@ -20,4 +20,29 @@ const queue: any[] = [
 // Create a securityCheck function that takes the queue as a parameter and returns a list of festivalgoers who can enter the festival
 // If guns are found, remove them and put them on the watchlist (only the names)
 // If alcohol is found confiscate it (set it to zero and add it to securityAlcholLoot) and let them enter the festival
+
+
+
+function securityCheck(list) {
+    list.forEach(elem => {
+        if (elem.guns > 0 && elem.alcohol === 0) {
+            elem.guns = 0;
+            watchlist.push(elem.name);
+        } else if (elem.guns === 0 && elem.alcohol > 0) {
+            securityAlcoholLoot += elem.alcohol;
+            elem.alcohol = 0;
+        } else if (elem.guns > 0 && elem.alcohol > 0) {    
+            securityAlcoholLoot += elem.alcohol;
+            elem.alcohol = 0;
+            elem.guns = 0;
+            watchlist.push(elem.name);
+        }
+    });
+    console.log(list);
+    console.log(watchlist);
+    console.log(securityAlcoholLoot)
+};
+
+securityCheck(queue);
+
 export = securityCheck;
