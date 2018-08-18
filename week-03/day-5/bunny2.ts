668ff753888f8c5d5254f35b7a3a2f3b392165f8
Kamilla Kovacs <knkovacs@gmail.com>
2018-08-18 18:13:55 +0200

    new(bunny2): set up if else if else statement for bunny list

---
+++ week-03/day-5/bunny2.ts
@@ -0,0 +1,13 @@
+function bunny2(numberOfBunnies) {
+  if (numberOfBunnies === 1) {
+    return numberOfBunnies;
+  } else if (numberOfBunnies % 2 === 0) {
+
+  } else {
+    
+  }
+}
+
+bunny2(10);
+
+
