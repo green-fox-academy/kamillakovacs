f353a2ac381e18ebd8c82bb042d976dbbaadfa28
Kamilla Kovacs <knkovacs@gmail.com>
2018-08-22 16:43:12 +0200

    feat(reservation): created function to run a set number of bookings and print each

--- week-04/day-3/reservation/reservation.ts
+++ week-04/day-3/reservation/reservation.ts
@@ -2,27 +2,32 @@ import { Reservationy } from "./reservationy";
 
 'use strict'
 
-export class Reservation implements Reservationy {
+export class Reservation implements Reservationy {  
   getDowBooking(): string {
-    let characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let dOW: string[] = ['MON', 'TUES', 'WED', 'THUR', 'FRI', 'SAT', 'SUN'];
+    let bookingDay = dOW[Math.floor((Math.random()*dOW.length))];
+    return bookingDay;
+  }  
+  
+  getCodeBooking(): string {
+    let characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
     let randomString: string = '';
     for (let i = 0; i < 8; i++) {
       randomString += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     let bookingNumber = randomString.slice(0, 8);
-    let randomDay = dOW[Math.floor((Math.random()*dOW.length))];
-    return `Booking# ${bookingNumber} for ${randomDay}`;
-    
-  }  
-  
-  getCodeBooking(): string {
-    return `Booking# ${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}`;
+    return bookingNumber;
   }
 
+  getBooking() {
+    for(let i = 0; i < numberOfBookings; i++) {
+    console.log(`Booking# ${newRes.getCodeBooking()} for ${newRes.getDowBooking()}`);
+    }
+  }
 
 }
 
+let numberOfBookings: number = 10;
 let newRes: Reservation = new Reservation;
 
-console.log(newRes.getDowBooking());
+newRes.getBooking();
