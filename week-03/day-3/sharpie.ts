a21963c51a1e67becb320ccb6996a0eec55ad735
Kamilla Kovacs <knkovacs@gmail.com>
2018-08-23 16:52:00 +0200

    feat(sharpie): created sharpie class with properties constructor and use method

---
+++ week-03/day-3/sharpie.ts
@@ -0,0 +1,16 @@
+class Sharpie {
+  color: string;
+  width: number;
+  inkAmount: number;
+
+  constructor(color: string, width: number) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  }
+
+  use() {
+    this.inkAmount--;
+    return this.inkAmount;
+  }
+}
