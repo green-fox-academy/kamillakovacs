4fba9a38ee3f3f1458721520d7cc7c204be7bcbc
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-12 12:10:34 +0200

    feat(queryselector1): two first exercises

---
+++ week-06/day-3/queryselector1.js
@@ -0,0 +1,8 @@
+// 1
+let king = document.getElementById('b325')
+console.log(king);
+
+// 2
+let businessLamp = document.querySelectorAll('p.asteroid, div.asteroid.b329')
+console.log(businessLamp)
+
