ca24668a7565f2c5ad3b4435be328e54a0547026
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-12 12:26:39 +0200

    feat(queryselector1): finished all

--- week-06/day-3/queryselector1.js
+++ week-06/day-3/queryselector1.js
@@ -1,8 +1,16 @@
 // 1
-let king = document.getElementById('b325')
+let king = document.getElementById('b325');
 console.log(king);
 
 // 2
-let businessLamp = document.querySelectorAll('p.asteroid, div.asteroid.b329')
-console.log(businessLamp)
+let businessLamp = document.querySelectorAll('p.asteroid, div.asteroid.b329');
+console.log(businessLamp);
 
+// 3
+let conceitedKing = document.querySelectorAll('div#b325, div.b326');
+console.log(conceitedKing[0]);
+console.log(conceitedKing[1]);
+
+// 4
+let noBusiness = document.querySelectorAll('div#b325, div.b326, div.b329');
+console.log(noBusiness);
