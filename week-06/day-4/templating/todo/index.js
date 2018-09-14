2e4c4343acb40438e5195729b6cd348b2b32a7b2
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-14 11:17:34 +0200

    set up flex box structure

---
+++ week-06/day-4/templating/todo/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const todos = [
+  'get up',
+  'survive',
+  'go back to bed',
+];
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    data: todos,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is up and running on port ${PORT}`);
+});
