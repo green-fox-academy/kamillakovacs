e10d686a36d2a4e70d70ecc2aa093215bcb9cabe
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-17 12:26:27 +0200

    feat(greeter) all three options working

--- week-07/day-1/frontend/app.js
+++ week-07/day-1/frontend/app.js
@@ -21,9 +21,27 @@ app.get('/doubling', (req, res) => {
       result: thisNumber * 2,
     });
   } else {
-    res.json({ error: "Please provide an input!"}) 
+    res.json({ error: "Please provide an input!"}); 
   }  
 });
+
+app.get('/greeter/', (req, res) => {
+  let studentName = req.query.name;
+  let studentTitle = req.query.title;
+  if (req.query.name && req.query.title) {
+    res.json({
+      welcome_message: `Oh, hi there ${studentName}, my dear student!`,
+    });
+  } else if (req.query.title) {
+    res.json({ error: "Please provide a name!"});
+  } else if (req.query.name) {
+    res.json({ error: "Please provide a title!"});
+  }
+});
+
+app.get('/appenda/{appendable}', (req, res) => {
+  
+})
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on port ${PORT}`);
