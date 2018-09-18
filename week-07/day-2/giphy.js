3e57caf30e32185a30fa49409dd9fb4052d84df1
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-18 14:50:45 +0200

    feat(giphy): giphy API search works

---
+++ week-07/day-2/giphy.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+const fetch = require('node-fetch');
+
+app.use('/assets', express.static('assets'));
+
+app.get('/api/giphy/', (req, res) => {
+  let url = [
+    'https://api.giphy.com/v1/gifs/search',
+    `?api_key=8zlJLJgPG8RMMWFNtllRrrWTFEBktjh0`,
+    `&q=kittens&limit=16`,
+  ].join('');
+
+  fetch(url)
+  .then((resp) => (resp.json()))
+  .then(response => {
+    res.json(response);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is up and running on port ${PORT}`);
+});
