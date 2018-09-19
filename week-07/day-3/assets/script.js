2acc4d0489ba7684bc0561db3d112e04623ae0e5
Kamilla Kovacs <knkovacs@gmail.com>
2018-09-19 17:50:39 +0200

    feat(bookstore): table sorta

--- week-07/day-3/assets/script.js
+++ week-07/day-3/assets/script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const host = 'http://localhost:3000/api';
   const http = new XMLHttpRequest();
-  const bookstore = document.querySelector('#book-names');
+  const bookstore = document.querySelector('#books');
   http.open('GET', `${host}/booknames/`, true);
   console.log(http);
   
@@ -10,12 +10,29 @@ window.onload = () => {
       const bookNameList = JSON.parse(http.response).result;
       console.log(bookNameList)
       bookNameList.forEach((bookName) => {
-        const newBookName = document.createElement('div');
-        bookstore.appendChild(newBookName);
+        const newTR = document.createElement('tr');
+        const newBookName = document.createElement('td');
+        const newBookAuthor = document.createElement('td');
+        const newBookCat = document.createElement('td');
+        const newBookPub = document.createElement('td');
+        const newBookPrice = document.createElement('td');
+
+        newTR.appendChild(newBookName);
+        newTR.appendChild(newBookAuthor);
+        newTR.appendChild(newBookCat);
+        newTR.appendChild(newBookPub);
+        newTR.appendChild(newBookPrice);
+  
+        bookstore.appendChild(newTR);
+        
         newBookName.innerText = bookName.book_name;
-      });
+        newBookAuthor.innerText = bookName.aut_name;
+        newBookCat.innerText = bookName.cate_descrip;
+        newBookPub.innerText = bookName.pub_name;
+        newBookPrice.innerText = bookName.book_price;
+      }); 
     }
   }
   http.send();
 }
-  
+
--- week-07/day-3/bookstore.js
+++ week-07/day-3/bookstore.js
@@ -17,8 +17,12 @@ const conn = mysql.createConnection ({
 app.use('/assets', express.static('assets'));
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
 app.get('/api/booknames', (req, res) => {
-  conn.query(`SELECT book_name from book_mast`, (err,result) => {
+  conn.query(`SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id;`, (err,result) => {
     if (err) {
       res.json({
         error: err.message,
